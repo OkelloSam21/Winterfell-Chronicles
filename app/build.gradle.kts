abf911dc0b77e583772bf425a6e097af9c5a686f
Okello Sam <samuelokello929@gmail.com>
2024-01-03 16:31:38 +0300

    Add hitl dependancies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id ("com.google.devtools.ksp")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -47,23 +49,16 @@ android {
     }
     applicationVariants.all {
         addJavaSourceFoldersToModel(
-            File(buildDir, "generated/ksp/$name/kotlin")
+            File(buildDir, "generated/ksp/$name/kotlin"),
         )
     }
-//    applicationVariants.configureEach { variant ->
-//        kotlin.sourceSets {
-//            getByName(variant.name) {
-//                kotlin.srcDir("build/generated/ksp/${variant.name}/kotlin")
-//            }
-//        }
-//    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -84,10 +79,14 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // coil
-    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("io.coil-kt:coil-compose:2.5.0")
 
     // Compose destination
-    implementation ("io.github.raamcosta.compose-destinations:core:1.8.42-beta")
-    ksp ("io.github.raamcosta.compose-destinations:ksp:1.8.42-beta")
+    implementation("io.github.raamcosta.compose-destinations:core:1.9.55")
+    ksp("io.github.raamcosta.compose-destinations:ksp:1.9.55")
 
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-android-compiler:2.50")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 }
