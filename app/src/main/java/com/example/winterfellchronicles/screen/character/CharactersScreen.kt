2f7866fbedf3ec44688d7894978eb9a2cf50bc7a
Okello Sam <samuelokello929@gmail.com>
2024-01-04 14:31:29 +0300

    mvvm

--- app/src/main/java/com/example/winterfellchronicles/screen/character/CharactersScreen.kt
+++ app/src/main/java/com/example/winterfellchronicles/screen/character/CharactersScreen.kt
@@ -1,15 +1,14 @@
-package com.example.winterfellchronicles.screen
+package com.example.winterfellchronicles.screen.character
 
-import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
@@ -18,26 +17,19 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
-import com.example.winterfellchronicles.data.remote.RetrofitInstance
 import com.example.winterfellchronicles.data.remote.WinterFellResponse
 import com.example.winterfellchronicles.screen.destinations.CharacterInfoDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 @RootNavGraph(start = true)
 @Destination
@@ -45,38 +37,48 @@ import retrofit2.Response
 @Composable
 fun CharactersScreen(
     navigator: DestinationsNavigator,
+    viewModel: CharacterViewModel = hiltViewModel(),
 ) {
     Scaffold(topBar = {
-        TopAppBar(title = { Text(text = "Winter-fell Chronicles") })
+        TopAppBar(title = { Text(text = "Characters") })
     }) { paddingValues ->
-        val context = LocalContext.current
-        var winterFellResponse by remember {
-            mutableStateOf<List<WinterFellResponse>?>(null)
-        }
+        val winterfellCharacterUiState = viewModel.winterFellCharacterUiState.collectAsState().value
 
-        RetrofitInstance.api.getCharacters().enqueue(object : Callback<List<WinterFellResponse>> {
-            override fun onResponse(
-                call: Call<List<WinterFellResponse>>,
-                response: Response<List<WinterFellResponse>>,
+        Box(modifier = Modifier.padding(paddingValues)) {
+            if (winterfellCharacterUiState.isLoading) {
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center),
+                )
+            } else if (
+                !winterfellCharacterUiState.isLoading &&
+                winterfellCharacterUiState.error == null &&
+                winterfellCharacterUiState.data.isNotEmpty()
             ) {
-                Toast.makeText(context, "API call was successful", Toast.LENGTH_SHORT).show()
-                winterFellResponse = response.body()
-            }
-
-            override fun onFailure(call: Call<List<WinterFellResponse>>, t: Throwable) {
-                Toast.makeText(context, "API call failed", Toast.LENGTH_SHORT).show()
-            }
-        })
-        if (!winterFellResponse.isNullOrEmpty()) {
-            LazyColumn(modifier = Modifier.padding(paddingValues)) {
-                items(winterFellResponse!!) { winterfell ->
-                    CharactersCard(winterFellResponse = winterfell, onClick = {
-                        navigator.navigate(CharacterInfoDestination(id = it))
-                    })
+                LazyColumn {
+                    items(winterfellCharacterUiState.data.size) { character ->
+                        CharactersCard(
+                            winterFellResponse = winterfellCharacterUiState.data[character],
+                            onClick = {
+                                navigator.navigate(CharacterInfoDestination(id = character))
+                            },
+                        )
+                    }
                 }
+            } else if (
+                !winterfellCharacterUiState.isLoading &&
+                winterfellCharacterUiState.error == null &&
+                winterfellCharacterUiState.data.isEmpty()
+            ) {
+                Text(
+                    modifier = Modifier.align(Alignment.Center),
+                    text = "No Characters Found",
+                )
+            } else {
+                Text(
+                    modifier = Modifier.align(Alignment.Center),
+                    text = winterfellCharacterUiState.error ?: "Unknown Error",
+                )
             }
-        } else {
-            CircularProgressIndicator()
         }
     }
 }
