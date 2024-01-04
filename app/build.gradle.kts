42debde98a50df9604431f72206ae1f3c5d27abf
Okello Sam <samuelokello929@gmail.com>
2024-01-04 14:30:56 +0300

    add hilt dependancies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.example.winterfellchronicles"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.winterfellchronicles"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
