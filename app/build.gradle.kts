e54b99ae0d133864f1ce3aee035baf22372ba718
Okello Sam <samuelokello929@gmail.com>
2024-01-29 13:55:56 +0300

    add: okhttp logger to show logs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,4 +89,6 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-android-compiler:2.50")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
--- app/src/main/java/com/example/winterfellchronicles/di/AppModule.kt
+++ app/src/main/java/com/example/winterfellchronicles/di/AppModule.kt
@@ -6,6 +6,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -15,10 +17,27 @@ import javax.inject.Singleton
 object AppModule {
     @Provides
     @Singleton
-    fun providesRetrofit(): Retrofit {
+    fun providesLogger(): HttpLoggingInterceptor{
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(logger: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(client: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://thronesapi.com/api/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
     }
 
